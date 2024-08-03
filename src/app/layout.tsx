6b89e8afb5d3cdb499d7004c5e99cf36e550fbdf
Olya <olapancisina1@gmail.com>
2024-08-03 16:19:39 +0200

    add meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Image from 'next/image'
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,7 +17,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={` ${montserrat.className} bg-[#50584B]` }>{children}</body>
+      <head>
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '534410672624192');
+            fbq('track', 'PageView');
+          `
+        }} />
+        <noscript>
+          <Image height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=534410672624192&ev=PageView&noscript=1" alt=""/>
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
+      <body className={`${montserrat.className} bg-[#50584B]`}>
+        {children}
+      </body>
     </html>
   );
 }
